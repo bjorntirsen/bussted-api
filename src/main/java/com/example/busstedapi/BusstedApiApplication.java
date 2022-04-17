0952bda066aa38b7cfb05977c0e59b2cc9136181
Björn Tirsén <bjorntirsen@gmail.com>
2022-04-17 15:19:32 +0200

    Inititalize project with Spring Boot

---
+++ src/main/java/com/example/busstedapi/BusstedApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.busstedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusstedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusstedApiApplication.class, args);
+	}
+
+}
